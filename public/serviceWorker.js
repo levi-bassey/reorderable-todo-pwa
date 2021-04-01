0f137006d5b7b1dc4e3ae9f248f6d68f344a809f
Levi Bassey <programlevi@gmail.com>
2021-04-01 18:59:15 +0100

    temp images to manifest

---
+++ public/serviceWorker.js
@@ -0,0 +1,4 @@
+//This event is triggered once this file executes
+self.addEventListener("install", (event) => {
+  console.log("service worker installing");
+});
--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ let app = new App({
   target: document.body,
 });
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/serviceWorker.js")
+    .then(() => console.log("Service worker registered"))
+    .catch(() => console.log("Service worker registration "));
+}
+
 export default app;
