1736a17e32002dbc9cdf960171b5a0eb580d7064
Levi Bassey <programlevi@gmail.com>
2021-04-02 12:04:56 +0100

    Testing build

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,37 @@
+const staticCacheName = "statics";
+const assets = [
+  "/",
+  "index.html",
+  "index.css",
+  "favicon-32x32.png",
+  "manifest.json",
+  "images/bg-desktop-dark.jpg",
+  "images/bg-desktop-light.jpg",
+  "images/icon-check.svg",
+  "dist/index.js",
+  "dist/index.js.map",
+  "dist/App.svelte.js",
+  "dist/todoStore.js",
+  "dist/components/Checkbox.svelte.js",
+  "dist/components/Header.svelte.js",
+  "dist/components/TodoItem.svelte.js",
+  "dist/components/TodoList.svelte.js",
+  "https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap",
+];
+
 //This event is triggered once this file executes
-self.addEventListener("install", (event) => {
-  console.log("service worker installing");
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches
+      .open(staticCacheName)
+      .then(cache => cache.addAll(assets).catch(console.log))
+  );
+});
+
+self.addEventListener("activate", event => {
+  console.log("service worker is activating");
+});
+
+self.addEventListener("fetch", event => {
+  console.log("fetched", event);
 });
